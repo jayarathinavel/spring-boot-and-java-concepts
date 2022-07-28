69bf856a3185c46762ad8a87dc0d802e0507ab38
jayarathinavel <file.one.storage@gmail.com>
2022-07-28 19:22:09 +0530

    Initializer Project

---
+++ src/main/java/com/jrv/springbootandjavaconcepts/SpringBootAndJavaConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.jrv.springbootandjavaconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndJavaConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndJavaConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrv/springbootandjavaconcepts/SpringBootAndJavaConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrv.springbootandjavaconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndJavaConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6c244411107d3a313a76f774ad63711e21ea8f70
jayarathinavel <file.one.storage@gmail.com>
2022-07-28 19:54:48 +0530

    Hello World

---
+++ src/main/java/com/jrv/springbootandjavaconcepts/MainController.java
@@ -0,0 +1,12 @@
+package com.jrv.springbootandjavaconcepts;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello World !";
+    }
+}

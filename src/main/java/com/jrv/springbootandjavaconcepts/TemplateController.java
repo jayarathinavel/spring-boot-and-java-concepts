6d9b5593a4cbcecbfb4bd3af888ebe4212ca46a3
jayarathinavel <jayrathinavel@gmail.com>
2022-11-07 11:37:25 +0530

    Changes in Students, HTML Changes

--- src/main/java/com/jrv/springbootandjavaconcepts/TemplateController.java
+++ src/main/java/com/jrv/springbootandjavaconcepts/TemplateController.java
@@ -1,20 +1,43 @@
 package com.jrv.springbootandjavaconcepts;
 
+import com.jrv.springbootandjavaconcepts.features.students.StudentController;
+import com.jrv.springbootandjavaconcepts.security.auth.ApplicationUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class TemplateController {
+    @Autowired
+    StudentController studentController;
+
+    @GetMapping
+    public String getIndexView(){
+        return "index";
+    }
 
     @GetMapping("login")
     public String getLoginView(){
-        return "login";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.isAuthenticated() && authentication.getPrincipal() != "anonymousUser"
+                ? "redirect:/":"login";
     }
 
     @GetMapping("login-success")
-    public String getLoginSuccess(){
+    public String getLoginSuccess(@AuthenticationPrincipal ApplicationUser applicationUser, Model model){
+        model.addAttribute("user", applicationUser);
         return "login-success";
+    }
+
+    @GetMapping("/views/students")
+    public String studentsView(Model model){
+        model.addAttribute("studentsList", studentController.getAllStudents());
+        return "students";
     }
 }

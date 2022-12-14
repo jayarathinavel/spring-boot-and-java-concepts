a43171e185cf4740fd6c0d94a8429c36e1ceecc9
jayarathinavel <jayrathinavel@gmail.com>
2022-12-14 17:10:27 +0530

    Removed Deprecated Spring Security, Thymeleaf Components

--- src/main/java/com/jrv/springbootandjavaconcepts/security/auth/ApplicationUserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.jrv.springbootandjavaconcepts.security.auth;
-
-import java.util.Optional;
-
-public interface ApplicationUserDAO {
-    Optional<ApplicationUser> selectApplicationUserByUserName(String username);
-}

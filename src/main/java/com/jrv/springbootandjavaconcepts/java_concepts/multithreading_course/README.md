3fd480f2f0354d81efee528dee8c671ebaf6b9bd
jayarathinavel <jayrathinavel@gmail.com>
2023-08-24 11:03:47 +0530

    Multithreading Course: Concurrency using Threads

---
+++ src/main/java/com/jrv/springbootandjavaconcepts/java_concepts/multithreading_course/README.md
@@ -0,0 +1,3 @@
+Course link - https://ibm-learning.udemy.com/course/parallel-and-asynchronous-programming-in-modern-java/
+Source Code - https://github.com/dilipsundarraj1/parallel-asynchronous-using-java/tree/master
+(Source Code and Slides are also downloaded separately from Udemy Course)
---
+++ src/main/java/com/jrv/springbootandjavaconcepts/java_concepts/multithreading_course/domain/Inventory.java
@@ -0,0 +1,14 @@
+package com.jrv.springbootandjavaconcepts.java_concepts.multithreading_course.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Inventory {
+    private int count;
+}
